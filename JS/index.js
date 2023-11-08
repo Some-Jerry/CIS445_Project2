9d51a4265e90a53cc24dca631c3b6f0227bd70ec
SomeJerry <321jerry.decker@gmail.com>
2023-11-08 16:13:29 -0600

    finished delete functionality, created .ejs files for insert function

--- JS/index.js
+++ JS/index.js
@@ -63,7 +63,15 @@ app.get("/deletedepartmentname", function (req, res) {
   res.render("deletedepartmentname");
 });
 
-app.post("/delete", function(req, res) {
+app.get("/deletedepartmentbuilding", function (req, res) {
+  res.render("deletedepartmentbuilding");
+});
+
+app.get("/deletedepartmentbudget", function (req, res) {
+  res.render("deletedepartmentbudget");
+});
+
+app.post("/deletedeptname", function(req, res) {
 
   var info = { dept_name: req.body.std_dept_name};
   var q = "DELETE FROM department WHERE ?";
@@ -81,24 +89,293 @@ app.post("/delete", function(req, res) {
 });
 });
 
-app.get("/deletedepartmentbuilding", function (req, res) {
-  res.render("deletedepartmentbuilding");
+app.post("/deletedeptbudget", function(req, res) {
+
+  var info = { budget: req.body.std_budget};
+  var q = "DELETE FROM department WHERE ?";
+  var success = true;
+
+  con.query(q, info, function(error, results) {
+  if (error) throw err; 
+  
+  if (results.affectedRows == 0) success = false;
+
+  console.log(results);
+
+  if (success) res.redirect("/querysuccess"); // redirect to success page
+  else res.redirect("/queryfailure"); // redirect to error page, query failed
+});
 });
 
-app.get("/deletedepartmentbudget", function (req, res) {
-  res.render("deletedepartmentbudget");
+app.post("/deletedeptbuilding", function(req, res) {
+
+  var info = { building: req.body.std_building};
+  var q = "DELETE FROM department WHERE ?";
+  var success = true;
+
+  con.query(q, info, function(error, results) {
+  if (error) throw err; 
+  
+  if (results.affectedRows == 0) success = false;
+
+  console.log(results);
+
+  if (success) res.redirect("/querysuccess"); // redirect to success page
+  else res.redirect("/queryfailure"); // redirect to error page, query failed
+});
 });
 
 app.get("/deleteinstructor", function (req, res) {
   res.render("deleteinstructor");
 });
 
+app.get("/deleteinstructorID", function (req, res) {
+  res.render("deleteinstructorID");
+});
+
+app.get("/deleteinstructorname", function (req, res) {
+  res.render("deleteinstructorname");
+});
+
+app.get("/deleteinstructordept", function (req, res) {
+  res.render("deleteinstructordept");
+});
+
+app.get("/deleteinstructorsalary", function (req, res) {
+  res.render("deleteinstructorsalary");
+});
+
+app.post("/deleteinstID", function(req, res) {
+
+  var info = { ID: req.body.std_ID};
+  var q = "DELETE FROM instructor WHERE ?";
+  var success = true;
+
+  con.query(q, info, function(error, results) {
+  if (error) throw err; 
+  
+  if (results.affectedRows == 0) success = false;
+
+  console.log(results);
+
+  if (success) res.redirect("/querysuccess"); // redirect to success page
+  else res.redirect("/queryfailure"); // redirect to error page, query failed
+});
+});
+
+app.post("/deleteinstname", function(req, res) {
+
+  var info = { name: req.body.std_name};
+  var q = "DELETE FROM instructor WHERE ?";
+  var success = true;
+
+  con.query(q, info, function(error, results) {
+  if (error) throw err; 
+  
+  if (results.affectedRows == 0) success = false;
+
+  console.log(results);
+
+  if (success) res.redirect("/querysuccess"); // redirect to success page
+  else res.redirect("/queryfailure"); // redirect to error page, query failed
+});
+});
+
+app.post("/deleteinstdept", function(req, res) {
+
+  var info = { dept_name: req.body.std_dept_name};
+  var q = "DELETE FROM instructor WHERE ?";
+  var success = true;
+
+  con.query(q, info, function(error, results) {
+  if (error) throw err; 
+  
+  if (results.affectedRows == 0) success = false;
+
+  console.log(results);
+
+  if (success) res.redirect("/querysuccess"); // redirect to success page
+  else res.redirect("/queryfailure"); // redirect to error page, query failed
+});
+});
+
+app.post("/deleteinstsalary", function(req, res) {
+
+  var info = { salary: req.body.std_salary};
+  var q = "DELETE FROM instructor WHERE ?";
+  var success = true;
+
+  con.query(q, info, function(error, results) {
+  if (error) throw err; 
+  
+  if (results.affectedRows == 0) success = false;
+
+  console.log(results);
+
+  if (success) res.redirect("/querysuccess"); // redirect to success page
+  else res.redirect("/queryfailure"); // redirect to error page, query failed
+});
+});
+
 app.get("/deletestudent", function (req, res) {
   res.render("deletestudent");
 });
 
+app.get("/deletestudentID", function (req, res) {
+  res.render("deletestudentID");
+});
+
+app.get("/deletestudentname", function (req, res) {
+  res.render("deletestudentname");
+});
+
+app.get("/deletestudentdept", function (req, res) {
+  res.render("deletestudentdept");
+});
+
+app.get("/deletestudentcredits", function (req, res) {
+  res.render("deletestudentcredits");
+});
+
+app.post("/deletestudID", function(req, res) {
+
+  var info = { ID: req.body.std_ID};
+  var q = "DELETE FROM student WHERE ?";
+  var success = true;
+
+  con.query(q, info, function(error, results) {
+  if (error) throw err; 
+  
+  if (results.affectedRows == 0) success = false;
+
+  console.log(results);
+
+  if (success) res.redirect("/querysuccess"); // redirect to success page
+  else res.redirect("/queryfailure"); // redirect to error page, query failed
+});
+});
+
+app.post("/deletestudname", function(req, res) {
+
+  var info = { name: req.body.std_name};
+  var q = "DELETE FROM student WHERE ?";
+  var success = true;
+
+  con.query(q, info, function(error, results) {
+  if (error) throw err; 
+  
+  if (results.affectedRows == 0) success = false;
+
+  console.log(results);
+
+  if (success) res.redirect("/querysuccess"); // redirect to success page
+  else res.redirect("/queryfailure"); // redirect to error page, query failed
+});
+});
+
+app.post("/deletestuddept", function(req, res) {
+
+  var info = { dept_name: req.body.std_dept};
+  var q = "DELETE FROM student WHERE ?";
+  var success = true;
+
+  con.query(q, info, function(error, results) {
+  if (error) throw err; 
+  
+  if (results.affectedRows == 0) success = false;
+
+  console.log(results);
+
+  if (success) res.redirect("/querysuccess"); // redirect to success page
+  else res.redirect("/queryfailure"); // redirect to error page, query failed
+});
+});
+
+app.post("/deletestudcredits", function(req, res) {
+
+  var info = { tot_credit: req.body.std_credits};
+  var q = "DELETE FROM student WHERE ?";
+  var success = true;
+
+  con.query(q, info, function(error, results) {
+  if (error) throw err; 
+  
+  if (results.affectedRows == 0) success = false;
+
+  console.log(results);
+
+  if (success) res.redirect("/querysuccess"); // redirect to success page
+  else res.redirect("/queryfailure"); // redirect to error page, query failed
+});
+});
+
 app.get("/deleteadvisor", function (req, res) {
   res.render("deleteadvisor");
+});
+
+app.get("/deleteadvisorIID", function (req, res) {
+  res.render("deleteadvisorIID");
+});
+
+app.get("/deleteadvisorSID", function (req, res) {
+  res.render("deleteadvisorSID");
+});
+
+app.post("/deleteadvSID", function(req, res) {
+
+  var info = { S_ID: req.body.std_SID};
+  var q = "DELETE FROM advisor WHERE ?";
+  var success = true;
+
+  con.query(q, info, function(error, results) {
+  if (error) throw err; 
+  
+  if (results.affectedRows == 0) success = false;
+
+  console.log(results);
+
+  if (success) res.redirect("/querysuccess"); // redirect to success page
+  else res.redirect("/queryfailure"); // redirect to error page, query failed
+});
+});
+
+app.post("/deleteadvIID", function(req, res) {
+
+  var info = { I_ID: req.body.std_IID};
+  var q = "DELETE FROM advisor WHERE ?";
+  var success = true;
+
+  con.query(q, info, function(error, results) {
+  if (error) throw err; 
+  
+  if (results.affectedRows == 0) success = false;
+
+  console.log(results);
+
+  if (success) res.redirect("/querysuccess"); // redirect to success page
+  else res.redirect("/queryfailure"); // redirect to error page, query failed
+});
+});
+// --- INSERT --- ///
+
+app.get("/insert", function (req, res) {
+  res.render("insert");
+});
+
+app.get("/insertadvisor", function (req, res) {
+  res.render("insertadvisor");
+});
+
+app.get("/insertdepartment", function (req, res) {
+  res.render("insertdepartment");
+});
+
+app.get("/insertstudent", function (req, res) {
+  res.render("insertstudent");
+});
+
+app.get("/insertinstructor", function (req, res) {
+  res.render("insertinstructor");
 });
 
 // --- MISC --- //
